d504091d7fa4a2c64203a240c573bb6f0ffe743c
Julie Etienne <julie.etienne11@hotmail.fr>
2017-03-01 15:41:52 +0100

    BacterieL changes in metabolism

--- BacterieL.cpp
+++ BacterieL.cpp
@@ -20,9 +20,9 @@ BacterieL::~BacterieL() {
 // ===========================================================================
 //                             Public Methods
 // ===========================================================================
-void BacterieL::metabolism(float& a_out) {
-    a_out *= (1 - rAA);
-    *A = a_out * rAA - (*A) * (1 - rAB);
+void BacterieL::metabolism(float& out) {
+    out *= (1 - rAA);
+    *A = out * rAA + (*A) * (1 - rAB);
     *B = (*A) * rAB + (*B);
 }
 
