a256d9fe1f2d9a587daca920c9b94534b106dc67
Chloé Daudier <Lokhia@MacBook-Air-de-Chloe.local>
2017-03-03 17:12:34 +0100

    case test

--- tests.cpp
+++ tests.cpp
@@ -10,11 +10,14 @@ using std::endl;
 #include "Bacteria.h"
 #include "BacterieL.h"
 #include "BacterieS.h"
+#include "Case.h"
 
 int main() {
 
     Bacteria* bacL = new BacterieL();
     Bacteria* bacS = new BacterieS();
+
+    Case case_ = Case(1);
 
     delete bacL;
     delete bacS;
