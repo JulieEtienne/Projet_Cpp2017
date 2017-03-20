650cfa9c21523f26902a28b6a13f2f99b7ca7d72
Julie Etienne <julie.etienne11@hotmail.fr>
2017-03-20 16:50:11 +0100

    Added initialize_grid() and constructor with param W, H

--- Environment.cpp
+++ Environment.cpp
@@ -1,25 +1,34 @@
 // ===========================================================================
 //                          Includes
 // ===========================================================================
-
 #include "Environment.h"
 
 
 // ===========================================================================
 //                             Constructors
 // ===========================================================================
-
 Environment::Environment()
 {
     W = 0;
     H = 0;
+    cout << "La grille est vide.\n" << endl;
 }
 
+Environment::Environment(int W_, int H_) {
+    W = W_;
+    H = H_;
+    initialize_grid();
+}
 
 // ===========================================================================
 //                             Destructor
 // ===========================================================================
-Environment::~Environment(){}
+Environment::~Environment(){
+    for (int i = 0; i < W; ++i) {
+        delete[] grid[i];
+    }
+    delete grid;
+}
 
 // ===========================================================================
 //                              Getters
@@ -33,19 +42,36 @@ int Environment::get_H() const
 {
     return H;
 }
-// ===========================================================================
-//                              Setters
-// ===========================================================================
-void Environment::set_W(int W_)
-{
-    W = W_;
-}
-
-void Environment::set_H(int H_)
-{
-    H = H_;
-}
 
 // ===========================================================================
 //                             Public Methods
 // ===========================================================================
+void Environment::initialize_grid(){
+    grid = new Case*[W];
+    for (int i = 0; i < W; ++i) {
+        grid[i] = new Case[H];
+    }
+}
+
+void Environment::search_gaps(){
+    //Parcourir la grille, quand une case est vide,
+    //get ses coordonnées, et appeler fill_gaps
+}
+
+void Environment::search_BestFitness(int x, int y){
+    //avec les coordonnées en argument (?),
+    //regarder la fitness des cases autour
+    //la comparer aux autres et renvoyer les coord de la meilleure
+    //si case autour est vide => fitness = 0
+}
+
+void Environment::fill_gaps(){
+    //parcourir les cases, si une est empty :
+    //trouver cellule avec meilleure fitness
+    //se divise et fille prend la case vide et l'autre prend la place
+    //de la mère
+}
+
+void Environment::diffusion(){
+    //diffusion des composés (code dans .pdf)
+}
