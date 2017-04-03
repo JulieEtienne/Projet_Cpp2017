a5a3b76e4be4c41017a7203c1897d08f237d0bda
Chloé Daudier <Lokhia@Air-de-Chloe.insa-lyon.fr>
2017-04-03 07:50:41 +0200

    update R script

--- results.R
+++ results.R
@@ -1,2 +1,22 @@
-data <- read.csv("resultats.csv",header = 'TRUE')
+data <- read.csv("resultats.csv",header = TRUE)
+data[1,1]
+A_init <- seq(0,50,10)
+t <- seq(1,1500,100)
+Lt <- c()
+St <- c()
+La <- c()
+Sa <- c()
+
+
+for ( i in 1:90)
+{
+  for (a in A_init)
+  {
+    if (data[i,3] == a)
+    {
+      Lt <- c(Lt, a)
+    }
+    print(Lt)
+  }
+}
 
